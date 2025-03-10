814e56801eea8866a5e7cb9c8fd1be5e00b67209
itaivincent <itaivincent321@gmail.com>
2025-03-10 17:28:24 +0200

    done

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Insert a single user
+        User::create([
+            'name' => 'Vincent', // Change name as needed
+            'email' => 'v.mhokore@techiserve.com', // Change email as needed
+            'password' => Hash::make('password123'), // Make sure the password is hashed
+        ]);
+    }
+}
