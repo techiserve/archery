0a908c055c94d13ce07b42cb771e78db139def53
itaivincent <itaivincent321@gmail.com>
2025-04-11 12:13:42 +0200

    toranaga sama

--- resources/views/events/scoring.blade.php
+++ resources/views/events/scoring.blade.php
@@ -68,11 +68,57 @@
                    </a>&nbsp;
                  
              @endif
-               
+             <button
+                              type="button"
+                              class="btn btn-info btn-sm"
+                              data-bs-toggle="modal"
+                              data-bs-target="#modalCente{{ $archer->id }}">
+                              Capture Bow & Arrow
+                            </button>
 
    
 <!-- Modal -->
 <div class="modal fade" id="modalCenter{{ $archer->id }}" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <form class="card-body" method="POST" action="{{ route('event.archerDetails') }}">
+      @csrf
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Capture Details</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="row g-6">
+            <div class="col mb-0">
+              <label class="form-label">Arrow Used</label>
+              <input type="text" name="arrowUsed" class="form-control" placeholder="Enter Arrow Used" />
+            </div>
+            <div class="col mb-0">
+              <label class="form-label">Bow Used</label>
+              <input type="text" name="bowUsed" class="form-control" placeholder="Enter Bow Used" />
+            </div>
+          </div>
+        
+        </div>
+
+        <!-- Send IDs or values properly -->
+        <input type="hidden" name="cat" value="{{ $cat }}">
+        <input type="hidden" name="archer" value="{{ $pple->id }}"> <!-- Better to use ID than surname -->
+        <input type="hidden" name="event" value="{{ $archer->event_id }}">
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
+
+   
+<!-- Modal -->
+<div class="modal fade" id="modalCente{{ $archer->id }}" tabindex="-1" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <form class="card-body" method="POST" action="{{ route('event.archerDetails') }}">
       @csrf
